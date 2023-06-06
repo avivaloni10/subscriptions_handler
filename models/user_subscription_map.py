6a2d7acb05eb6de76df82d9fdb3052d9bd3ef0e9
Aviv Aloni <aviv@vulcan.io>
2023-06-07 00:40:47 +0300

    Add on delete cascade.

--- models/user_subscription_map.py
+++ models/user_subscription_map.py
@@ -8,8 +8,8 @@ from config.db import Base
 class UserSubscriptionMap(Base):
     __tablename__ = "user_subscription_map"
 
-    subscription_name = Column(ForeignKey("subscriptions.name"), primary_key=True, nullable=False)
-    user_email = Column(ForeignKey("users.email"), primary_key=True, nullable=False)
+    subscription_name = Column(ForeignKey("subscriptions.name"), ondelete="CASCADE", primary_key=True, nullable=False)
+    user_email = Column(ForeignKey("users.email"), ondelete="CASCADE", primary_key=True, nullable=False)
     card_owner_id = Column(String, nullable=False)
     card_number = Column(String, nullable=False)
     cvv = Column(String, nullable=False)
